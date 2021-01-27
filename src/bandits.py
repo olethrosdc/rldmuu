883176973675ec0e29ca061ea7d82374196f3f1a
Christos Dimitrakakis <olethros@olethros-desktop.lan>
2021-01-27 11:46:06 +0100

    Bandit skeleton update

--- src/bandits.py
+++ src/bandits.py
@@ -6,12 +6,37 @@ import gym_bandits
 class AverageBanditAlgorithm:
     def __init__(self, n_actions):
         self.n_actions = n_actions
-        self.total_reward = np.zeros(n_actions)
-        self.n_pulls = np.zeros(n_actions)
+        self.total_reward = np.ones(n_actions)
+        self.n_pulls = np.ones(n_actions)
     def act(self):
-        ## FILL IN
-        return action
+        return argmax(total_reward./n_pulls)
+    def update(self, action, reward):
+        self.total_reward[action] += reward
+        self.n_pulls[action] += 1
+        
+class BayesOptimalBernoulliBandit:
+    def __init__(self, n_actions, horizon):
+        self.n_actions = n_actions
+        self.horizon = horizon # maximum steps to look ahead
+        ## Beta distribution parameters
+        self.alpha = np.ones(n_actions)
+        self.beta = np.ones(n_actions)
 
+    def recurse_utility(self, belief):
+        # careful here not to overwrite the actual self.alpha and self.beta when recursing
+        return max_utility
+    def calculate_utility(self, action):
+        # Calculate the expected return of the action by marginalising over rewards and next beliefs. This can be done by recursion
+    def act(self):
+        ## Fill in
+        U = np.zeros(n_actions)
+        for i in range(self.n_actions):
+            U[i] = calculate_utility(i)
+        return argmax(U)
+    def update(self, action, reward):
+        self.alpha[action] += reward
+        self.beta[action] += (1 - reward)
+    
 
 # Set up the bandit environment
 env = gym.make("BanditTenArmedGaussian-v0")
