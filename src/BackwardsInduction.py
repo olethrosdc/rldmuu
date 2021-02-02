a9abfebb38ad4612fb687b3a323b2ef9de425ad6
Christos Dimitrakakis <olethros@olethros-desktop.lan>
2021-02-02 11:31:55 +0100

    BI skeleton

---
+++ src/BackwardsInduction.py
@@ -0,0 +1,16 @@
+import MDP
+
+## Define algorithm
+def backwards_induction(mdp, T):
+    print ("Unimplemented")
+    return 0, 0, 0
+
+n_actions = 2
+n_states = 5
+T = 10
+mdp = MDP.DiscreteMDP(n_states, n_actions)
+policy, V, Q = backwards_induction(mdp, T)
+
+
+
+    
