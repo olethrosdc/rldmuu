1036567a420cbafb541ca0385bceeeb6afd9532c
Christos Dimitrakakis <olethros@olethros-desktop.lan>
2021-02-09 10:39:37 +0100

    Working bench

--- src/bandits.py
+++ src/bandits.py
@@ -2,6 +2,22 @@
 import gym
 ## This is a bandit environment
 import gym_bandits
+from gym import spaces
+from gym.utils import seeding
+import matplotlib.pyplot as plt
+
+import numpy as np
+
+
+def moving_average(x, K):
+  T = x.shape[0]
+  n = x.shape[1]
+  m = int(np.ceil(T / K))
+  y = np.zeros([m, n])
+  for alg in range(n):
+      for t in range(m):
+        y[t,alg] = np.mean(x[t*K:(t+1)*K, alg])
+  return y
 
 
 ## Here bandit problems are sampled from a Beta distribution
@@ -40,7 +56,7 @@ class AverageBanditAlgorithm:
         self.total_reward = np.ones(n_actions)
         self.n_pulls = np.ones(n_actions)
     def act(self):
-        return argmax(total_reward./n_pulls)
+        return np.argmax(self.total_reward/self.n_pulls)
     def update(self, action, reward):
         self.total_reward[action] += reward
         self.n_pulls[action] += 1
@@ -51,75 +67,57 @@ class StochasticBanditAlgorithm:
         self.mean = np.ones(n_actions)
         self.alpha = 0.01 * np.ones(n_actions)
     def act(self):
-        return argmax(mean)
+        return np.argmax(self.mean)
     ## Stochastic update: mu = mu + alpha * z
     ## z = r - mu
     def update(self, action, reward):
-        self.mean[alpha] += self.alpha[action] * (reward - mu[alpha])
+        self.mean[action] += self.alpha[action] * (reward - self.mean[action])
 
 
-class BayesOptimalBernoulliBandit:
-    def __init__(self, n_actions, horizon):
-        self.n_actions = n_actions
-        self.horizon = horizon # maximum steps to look ahead
-        ## Beta distribution parameters
-        self.alpha = np.ones(n_actions)
-        self.beta = np.ones(n_actions)
 
-    def recurse_utility(self, belief):
-        # careful here not to overwrite the actual self.alpha and self.beta when recursing
-        return max_utility
-    def calculate_utility(self, action):
-        # Calculate the expected return of the action by marginalising over rewards and next beliefs. This can be done by recursion
-    def act(self):
-        ## Fill in
-        U = np.zeros(n_actions)
-        for i in range(self.n_actions):
-            U[i] = calculate_utility(i)
-        return argmax(U)
-    def update(self, action, reward):
-        self.alpha[action] += reward
-        self.beta[action] += (1 - reward)
-    
 
-  
-for horizon in range(4):
-  algs = []
-  algs.append(AverageBanditAlgorithm)
-  algs.append(StochasticBanditAlgorithm)
-  n_algs = len(algs)
-  reward_t = np.zeros([T, n_algs])
-  total_reward = np.zeros([n_algs])
-  for experiment in range(n_experiments):
-    env = environments[experiment]
-    env.reset()
-    alg_index = 0
-    for Alg in algs:
-      alg = Alg(n_actions, horizon)
-      run_reward = 0
-      for i_episode in range(T):
-        observation = env.reset()
-        for t in range(100):
-          env.render()
-          action = alg.act()
-          observation, reward, done, info = env.step(action)
-          alg.update(action, reward)
-          run_reward += reward
-          reward_t[i_episode, alg_index] += reward
-          if done:
-            #            print("Episode finished after {} timesteps".format(t+1))
-            break
-      total_reward[alg_index] += run_reward
-      alg_index += 1
-      env.close()
+n_actions = 2
+n_experiments = 100
+T = 10000
+environments = []
+for experiment in range(n_experiments):
+  environments.append(BetaBandits(n_actions, 1, 1))
 
-  total_reward /= n_experiments
-  reward_t /= n_experiments
-  print("H:", horizon, "U:", total_reward)
-  plt.clf()
-  plt.plot(moving_average(reward_t, 10))
-  plt.legend(["Greedy", "Stochastic"])
-  plt.savefig("horizon" + str(horizon) + ".pdf")
+algs = []
+algs.append(AverageBanditAlgorithm)
+algs.append(StochasticBanditAlgorithm)
+n_algs = len(algs)
+reward_t = np.zeros([T, n_algs])
+total_reward = np.zeros([n_algs])
+for experiment in range(n_experiments):
+  env = environments[experiment]
+  env.reset()
+  alg_index = 0
+  for Alg in algs:
+    alg = Alg(n_actions)
+    run_reward = 0
+    for i_episode in range(T):
+      observation = env.reset()
+      for t in range(100):
+        env.render()
+        action = alg.act()
+        observation, reward, done, info = env.step(action)
+        alg.update(action, reward)
+        run_reward += reward
+        reward_t[i_episode, alg_index] += reward
+        if done:
+          #            print("Episode finished after {} timesteps".format(t+1))
+          break
+    total_reward[alg_index] += run_reward
+    alg_index += 1
+    env.close()
+
+total_reward /= n_experiments
+reward_t /= n_experiments
+plt.clf()
+plt.plot(moving_average(reward_t, 10))
+plt.legend(["Greedy", "Stochastic"])
+plt.savefig("stochastic.pdf")
 #  plt.show()
   
  
