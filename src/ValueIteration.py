7d607356aea94bc7aade091998ba0e8c983fe794
Christos Dimitrakakis <olethros@olethros-desktop.lan>
2021-02-09 10:34:27 +0100

    Added bandit algorithm

--- src/ValueIteration.py
+++ src/ValueIteration.py
@@ -6,37 +6,24 @@ def value_iteration(mdp, n_iterations, gamma):
     policy = np.zeros([mdp.n_states])
     V = np.zeros([mdp.n_states])
     Q = np.zeros([mdp.n_states, mdp.n_actions])
-    for t in range(n_iterations-1, -1, -1):
+    for t in range(n_iterations):
         for s in range(mdp.n_states):
             for a in range(mdp.n_actions):
-                if (t==n_iterations-1):
-                    Q[s,a,t] = mdp.get_reward(s, a)
-                else:
-                    P_sa = mdp.get_transition_probabilities(s, a)
-                    U_next = sum(P_sa * V[:,t+1])
-                    Q[s,a,t] = mdp.get_reward(s, a) + U_next
-            V[s,t] = max(Q[s,:,t])
-            policy[s, t] = np.argmax(Q[s,:,t])
+                P_sa = mdp.get_transition_probabilities(s, a)
+                U_next = sum(P_sa * V[:])
+                Q[s,a] = mdp.get_reward(s, a) + gamma * U_next
+            V[s] = max(Q[s,:])
+            policy[s] = np.argmax(Q[s,:])
+        print(V)
     return policy, V, Q
 
 n_actions = 2
 n_states = 2
 n_iterations = 1000
+gamma = 0.9
 mdp = MDP.DiscreteMDP(n_states, n_actions)
-policy, V, Q = backwards_induction(mdp, n_iterations)
+policy, V, Q = value_iteration(mdp, n_iterations, gamma)
 
-for s in range(mdp.n_states):
-    for a in range(mdp.n_actions):
-        print("S:", s, "A:", a, mdp.get_transition_probabilities(s,a))
-
-
-for t in range(n_iterations):
-    print(policy[:,t])
-        
-for t in range(n_iterations):
-    print(V[:,t])
-
-    
 
 
 
--- src/bandits.py
+++ src/bandits.py
@@ -3,6 +3,37 @@ import gym
 ## This is a bandit environment
 import gym_bandits
 
+
+## Here bandit problems are sampled from a Beta distribution
+class BetaBandits(gym.Env):
+  def __init__(self, bandits=10, alpha=1, beta=1):
+    self.r_dist = np.zeros(bandits)
+    for i in range(bandits):
+      self.r_dist[i] = np.random.beta(alpha, beta)
+    self.n_bandits = bandits
+    self.action_space = spaces.Discrete(self.n_bandits)
+    self.observation_space = spaces.Discrete(1)
+
+    self._seed()
+
+  def _seed(self, seed=None):
+    self.np_random, seed = seeding.np_random(seed)
+    return [seed]
+
+  def step(self, action):
+    assert self.action_space.contains(action)
+    done = True
+    reward = np.random.binomial(1, self.r_dist[action])
+    return 0, reward, done, {}
+
+  def reset(self):
+    return 0
+
+  def render(self, mode='human', close=False):
+    pass
+
+
+
 class AverageBanditAlgorithm:
     def __init__(self, n_actions):
         self.n_actions = n_actions
@@ -13,7 +44,20 @@ class AverageBanditAlgorithm:
     def update(self, action, reward):
         self.total_reward[action] += reward
         self.n_pulls[action] += 1
-        
+
+class StochasticBanditAlgorithm:
+    def __init__(self, n_actions):
+        self.n_actions = n_actions
+        self.mean = np.ones(n_actions)
+        self.alpha = 0.01 * np.ones(n_actions)
+    def act(self):
+        return argmax(mean)
+    ## Stochastic update: mu = mu + alpha * z
+    ## z = r - mu
+    def update(self, action, reward):
+        self.mean[alpha] += self.alpha[action] * (reward - mu[alpha])
+
+
 class BayesOptimalBernoulliBandit:
     def __init__(self, n_actions, horizon):
         self.n_actions = n_actions
@@ -38,22 +82,46 @@ class BayesOptimalBernoulliBandit:
         self.beta[action] += (1 - reward)
     
 
-# Set up the bandit environment
-env = gym.make("BanditTenArmedGaussian-v0")
-alg = AverageBanditAlgorithm(env.action_space.n)
-env.reset()
-total_reward = 0
-for i_episode in range(1000):
-    observation = env.reset()
-    for t in range(100):
-        env.render()
-        action = env.action_space.sample()
-        observation, reward, done, info = env.step(action)
-        print("A:, R:", action, reward)
-        total_reward += reward
-        if done:
-#            print("Episode finished after {} timesteps".format(t+1))
+  
+for horizon in range(4):
+  algs = []
+  algs.append(AverageBanditAlgorithm)
+  algs.append(StochasticBanditAlgorithm)
+  n_algs = len(algs)
+  reward_t = np.zeros([T, n_algs])
+  total_reward = np.zeros([n_algs])
+  for experiment in range(n_experiments):
+    env = environments[experiment]
+    env.reset()
+    alg_index = 0
+    for Alg in algs:
+      alg = Alg(n_actions, horizon)
+      run_reward = 0
+      for i_episode in range(T):
+        observation = env.reset()
+        for t in range(100):
+          env.render()
+          action = alg.act()
+          observation, reward, done, info = env.step(action)
+          alg.update(action, reward)
+          run_reward += reward
+          reward_t[i_episode, alg_index] += reward
+          if done:
+            #            print("Episode finished after {} timesteps".format(t+1))
             break
-env.close()
-print("total reward: ", total_reward)
+      total_reward[alg_index] += run_reward
+      alg_index += 1
+      env.close()
+
+  total_reward /= n_experiments
+  reward_t /= n_experiments
+  print("H:", horizon, "U:", total_reward)
+  plt.clf()
+  plt.plot(moving_average(reward_t, 10))
+  plt.legend(["Greedy", "Stochastic"])
+  plt.savefig("horizon" + str(horizon) + ".pdf")
+#  plt.show()
+  
+ 
+
 
