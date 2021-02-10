1e4735779b89fb79ece65f71e03d635b158ac8bb
Christos Dimitrakakis <olethros@olethros-desktop.lan>
2021-02-10 11:52:49 +0100

    QLearning

--- src/QLearning.py
+++ src/QLearning.py
@@ -1,12 +1,20 @@
 import numpy as np
 
 class QLearning:
-    def __init__(self, n_actions):
+    def __init__(self, n_actions, n_states, discount=0.9, alpha = 0.01):
         self.n_actions = n_actions
-        self.total_reward = np.ones(n_actions)
-        self.n_pulls = np.ones(n_actions)
+        self.n_states = n_states
+        self.Q = np.zeros([n_states, n_actions])
+        self.discount = discount
+        self.alpha = 0.01
     def act(self):
-        return np.argmax(self.total_reward/self.n_pulls)
-    def update(self, action, reward, observation):
-        self.total_reward[action] += reward
-        self.n_pulls[action] += 1
+        if (np.random.uniform() < self.epsilon):
+            return np.random.choice(self.n_actions)
+        return np.argmax(Q[self.state, :])
+    
+    def update(self, action, reward, state):
+        self.Q[self.state, action] += alpha * np.max(reward + self.discount * Q[state, :] - Q[self.state, action])
+        self.state = state
+
+    def reset(self, state):
+        self.state = state
