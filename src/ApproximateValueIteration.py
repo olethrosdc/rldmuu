e3ce20826243c41da4310663f7cb12c6dafb6609
Christos Dimitrakakis <olethros@olethros-desktop.lan>
2021-02-17 10:05:46 +0100

    AVI

---
+++ src/ApproximateValueIteration.py
@@ -0,0 +1,46 @@
+import MDP
+import numpy as np
+import ValueIteration
+
+## Define algorithm
+def approximate_value_iteration(mdp, n_iterations, gamma, representation_size = 4, n_samples = 4):
+    policy = np.zeros([mdp.n_states])
+    V = np.zeros([mdp.n_states])
+    Q = np.zeros([mdp.n_actions])
+    u = np.zeros(representation_size);
+    SampledStates = np.random.choice(mdp.n_states,100)
+    for t in range(n_iterations):
+        for s in SampledStates
+            for a in range(mdp.n_actions):
+                P_sa = mdp.get_transition_probabilities(s, a)
+                U_next = 0
+                ## MC approximation
+                for k in range(n_samples):
+                    # U_next = += V[next_state]
+                    next_state = np.random.choice(mdp.n_states, p = P_sa)
+                    U_next += u[next_state%representation_size]
+                U_next /= n_samples
+                Q[a] = mdp.get_reward(s, a) + gamma * U_next
+            V[s] = max(Q)
+            policy[s] = np.argmax(Q)
+            #print(V)
+        cnt = np.zeros(representation_size);
+        u = np.zeros(representation_size);
+        # Minimise \sum_s |V(s) - u(s)|^2 with SGD
+        for k in range(n_iterations):
+            for s in SampledStates:
+                s = np.random.choice(mdp.n_states)
+                s_hat = s % representation_size
+                u[s_hat] += 0.1 * (V[s] - u[s_hat])
+
+    return policy, V
+
+n_actions = 4
+n_states = 64
+n_iterations = 1000
+gamma = 0.9
+mdp = MDP.DiscreteMDP(n_states, n_actions)
+a_policy, a_V= approximate_value_iteration(mdp, n_iterations, gamma, 32)
+policy, V, Q= ValueIteration.value_iteration(mdp, n_iterations, gamma)
+print(V)
+print(a_V)
