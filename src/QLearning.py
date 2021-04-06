916aefdb661e629264052df73aa49db4ccd92447
Christos Dimitrakakis <olethros@olethros-desktop.lan>
2021-04-06 21:31:30 +0200

    Plan

--- src/QLearning.py
+++ src/QLearning.py
@@ -1,21 +1,23 @@
 import numpy as np
 
 class QLearning:
-    def __init__(self, n_actions, n_states, discount=0.9, alpha = 0.01, epsilon=0.1):
+    def __init__(self, n_actions, n_states, discount=0.9, alpha = 0.01, epsilon=0.1, decay = 0):
         self.n_actions = n_actions
         self.n_states = n_states
         self.Q = np.zeros([n_states, n_actions])
         self.discount = discount
-        self.alpha = 0.01
-        self.epsilon = 0.1
+        self.alpha = alpha
+        self.epsilon = epsilon
+        self.decay = decay
     def act(self):
-        self.epsilon = 1 / (1 / self.epsilon + 0.5)
+        self.epsilon = 1 / (1 / self.epsilon + self.decay)
         if (np.random.uniform() < self.epsilon):
             return np.random.choice(self.n_actions)
         return np.argmax(self.Q[self.state, :])
     
     def update(self, action, reward, state):
         self.Q[self.state, action] += self.alpha * np.max(reward + self.discount * self.Q[state, :] - self.Q[self.state, action])
+        self.alpha += 1 / ( self.alpha + self.decay)
         self.state = state
 
     def reset(self, state):
--- src/test_bench.py
+++ src/test_bench.py
@@ -14,7 +14,7 @@ def moving_average(x, K):
   return y
 
 
-n_experiments = 1
+n_experiments = 100
 T = 1000
 environments = []
 
@@ -22,45 +22,43 @@ environments.append(chain.Chain(5))
 
 
 algs = []
-algs.append(MDPBelief.SimpleModelBasedRL)
-algs.append(MDPBelief.SampleBasedRL)
+algs.append(QLearning.QLearning)
 n_algs = len(algs)
-reward_t = np.zeros([T, n_algs])
-total_reward = np.zeros([n_algs])
-for experiment in range(n_experiments):
-  env = environments[experiment]
-  env.reset()
-  alg_index = 0
-  for Alg in algs:
-    alg = Alg(env.observation_space.n, env.action_space.n)
-    run_reward = 0
-    for i_episode in range(1):
-      observation = env.reset()
-      alg.reset(observation)
-      for t in range(T):
-        env.render()
-        action = alg.act()
-        #print(observation, action)
-        observation, reward, done, info = env.step(action)
-        alg.update(action, reward, observation)
-        run_reward += reward
-        reward_t[i_episode, alg_index] += reward
-        if done:
-          #            print("Episode finished after {} timesteps".format(t+1))
-          break
-    total_reward[alg_index] += run_reward
-    alg_index += 1
-    env.close()
 
+alpha = 0.4
+epsilon = 0.3
+decay = 0.1
+for decay in np.arange(0,1,0.1):
+  reward_t = np.zeros([T, n_algs])
+  total_reward = np.zeros([n_algs])
+  for experiment in range(n_experiments):
+    env = environments[0];#experiment]
+    env.reset()
+    alg_index = 0
+    for Alg in algs:
+      alg = Alg(n_states = env.observation_space.n, n_actions = env.action_space.n, alpha = alpha, epsilon = epsilon, decay = decay)
+      run_reward = 0
+      for i_episode in range(1):
+        observation = env.reset()
+        alg.reset(observation)
+        for t in range(T):
+          env.render()
+          action = alg.act()
+          #print(observation, action)
+          observation, reward, done, info = env.step(action)
+          alg.update(action, reward, observation)
+          run_reward += reward
+          reward_t[i_episode, alg_index] += reward
+          if done:
+            #            print("Episode finished after {} timesteps".format(t+1))
+            break
+      total_reward[alg_index] += run_reward
+      alg_index += 1
+      env.close()
+  total_reward /= n_experiments
+  reward_t /= n_experiments
+  print(alpha, epsilon, decay, total_reward)
 
-total_reward /= n_experiments
-reward_t /= n_experiments
-print(total_reward)
-plt.clf()
-plt.plot(moving_average(reward_t, 10))
-plt.legend(["QLearning"])
-plt.savefig("qlearning.pdf")
-#  plt.show()
   
  
 
