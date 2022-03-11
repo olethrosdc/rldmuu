aea6589ed5891ab41a99ae3d624c482be384c21f
Christos Dimitrakakis <christos.dimitrakakis@gmail.com>
2022-03-11 09:54:44 +0100

    Backwards induction definition

--- src/RL/BackwardsInduction.py
+++ src/RL/BackwardsInduction.py
@@ -7,16 +7,7 @@ def backwards_induction(mdp, T):
     V = np.zeros([mdp.n_states, T])
     Q = np.zeros([mdp.n_states, mdp.n_actions, T])
     for t in range(T-1, -1, -1):
-        for s in range(mdp.n_states):
-            for a in range(mdp.n_actions):
-                if (t==T-1):
-                    Q[s,a,t] = mdp.get_reward(s, a)
-                else:
-                    P_sa = mdp.get_transition_probabilities(s, a)
-                    U_next = sum(P_sa * V[:,t+1])
-                    Q[s,a,t] = mdp.get_reward(s, a) + U_next
-            V[s,t] = max(Q[s,:,t])
-            policy[s, t] = np.argmax(Q[s,:,t])
+
     return policy, V, Q
 
 n_actions = 2
