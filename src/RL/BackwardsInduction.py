1ec5f2755053c7d07f0d0cfbb0966a4ba29a1cec
Christos Dimitrakakis <christos.dimitrakakis@gmail.com>
2022-03-15 08:55:00 +0100

    Assingment of papers

--- src/RL/BackwardsInduction.py
+++ src/RL/BackwardsInduction.py
@@ -7,7 +7,14 @@ def backwards_induction(mdp, T):
     V = np.zeros([mdp.n_states, T])
     Q = np.zeros([mdp.n_states, mdp.n_actions, T])
     for t in range(T-1, -1, -1):
-
+        for s in range(mdp.n_states):
+            for a in range(mdp.n_actions):
+                if (t==T-1):
+                    Q[s,a,t] = mdp.get_reward(s,a)
+                else:
+                    Q[s,a,t] = mdp.get_reward(s,a) + sum([V[s2, t+1] * mdp.get_transition_probability(s,a,s2) for s2 in range(mdp.n_states)])
+            policy[s,t] = np.argmax(Q[s,:,t])
+            V[s,t] = max(Q[s,:,t])
     return policy, V, Q
 
 n_actions = 2
