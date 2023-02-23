5f064654252c204ae82d980b01beb3e3510f12c5
Christos Dimitrakakis <christos.dimitrakakis@gmail.com>
2023-02-23 12:48:22 +0100

    Paradox comments

--- src/beliefs_and_decisions/paradox.py
+++ src/beliefs_and_decisions/paradox.py
@@ -14,18 +14,29 @@ def linear_utility(money):
     return money
 
 
-# there is not always a closed-form solution for the expected utility,
-# hence we are doing a numerical approximation
+def quadratic_utility(money:
+     ## fill in
+return money                    
+
+
+# There is not always a closed-form solution for the expected utility,
+# hence we are doing a numerical approximation.  In your function, you
+# should calcualte the expected utility of playing, depending on the
+# payment and the starting capital.
+#
+# For the standard scenario with linear utilities, the approximation
+# will always strongly underestimate the utility.
 def expected_utility(starting_capital, payment, utility_function):
     EU = 0
-    p_stop = 0.5
-    gain = 2
-    for stopping in range(100):
+    p_stop = 0.5 # the probability of stopping at any step
+    gain = 2 # the gain in money for every step that passes
+    # here, you should calculate the total expected utility up to some point
+    for stopping in range(1000):
     # fill in
     return EU
 
 utility_function = log_utility
-starting_capital = 100
+starting_capital = 10
 
 for payment in range(starting_capital):
     no_play_util = utility_function(payment)
