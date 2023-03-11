adffd9a72c4ec32c896d4cf345c3c82e29fd6377
Christos Dimitrakakis <christos.dimitrakakis@gmail.com>
2023-03-11 10:57:57 +0100

    Removed implementation

--- src/RL/BackwardsInduction.py
+++ src/RL/BackwardsInduction.py
@@ -6,15 +6,8 @@ def backwards_induction(mdp, T):
     policy = np.zeros([mdp.n_states, T])
     V = np.zeros([mdp.n_states, T])
     Q = np.zeros([mdp.n_states, mdp.n_actions, T])
-    for t in range(T-1, -1, -1):
-        for s in range(mdp.n_states):
-            for a in range(mdp.n_actions):
-                if (t==T-1):
-                    Q[s,a,t] = mdp.get_reward(s,a)
-                else:
-                    Q[s,a,t] = mdp.get_reward(s,a) + sum([V[s2, t+1] * mdp.get_transition_probability(s,a,s2) for s2 in range(mdp.n_states)])
-            policy[s,t] = np.argmax(Q[s,:,t])
-            V[s,t] = max(Q[s,:,t])
+    ## to fill in
+    
     return policy, V, Q
 
 n_actions = 2
