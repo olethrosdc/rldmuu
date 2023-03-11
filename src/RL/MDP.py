7b6ae0f62c5103bc9d1bcac1fcb854d21c459ec6
Christos Dimitrakakis <christos.dimitrakakis@gmail.com>
2023-03-11 19:16:33 +0100

    Comments

--- src/RL/MDP.py
+++ src/RL/MDP.py
@@ -16,13 +16,14 @@ class DiscreteMDP:
                 self.P[s,a] = np.random.dirichlet(np.ones(n_states)) # generalisation of Beta to multiple outcome
                 self.R[s,a] = np.round(np.random.uniform(), decimals=1)
                     
-    # get a single P(j|s,a)
+    # get the probability of next state j given current state s, action a, i.e. P(j|s,a)
     def get_transition_probability(self, state, action, next_state):
         return self.P[state,action,next_state]
-    # get the vector P( . | s,a)
+    # get the vector of probabilities over next states P( . | s,a)
     def get_transition_probabilities(self, state, action):
         return self.P[state,action]
-    # get the reward for the current state action
+    # Get the reward for the current state action.
+    # It can also be interpreted as the expected reward for the state and action.
     def get_reward(self, state, action):
         return self.R[state, action]
 
