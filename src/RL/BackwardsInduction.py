9017973e8602238c00e3d5135173aed69af3f017
Christos Dimitrakakis <christos.dimitrakakis@gmail.com>
2023-03-14 10:37:07 +0100

    Reduced

--- src/RL/BackwardsInduction.py
+++ src/RL/BackwardsInduction.py
@@ -2,19 +2,20 @@ import MDP
 import numpy as np
 
 ## Define algorithm
-def backwards_induction(mdp, T):
+## policy[state, time] gives you index of the action played at any state for any time.
+def backwards_induction(mdp, policy, T):
     policy = np.zeros([mdp.n_states, T])
     V = np.zeros([mdp.n_states, T])
-    Q = np.zeros([mdp.n_states, mdp.n_actions, T])
+    #Q = np.zeros([mdp.n_states, mdp.n_actions, T])
     ## to fill in
     
-    return policy, V, Q
+    return policy, V
 
 n_actions = 2
 n_states = 2
 T = 1000
 mdp = MDP.DiscreteMDP(n_states, n_actions)
-policy, V, Q = backwards_induction(mdp, T)
+policy, V = backwards_induction(mdp, T)
 
 for s in range(mdp.n_states):
     for a in range(mdp.n_actions):
