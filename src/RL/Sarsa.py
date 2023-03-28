625b79eeaf6b4cfa36882592a7e9e57b31e44530
Christos Dimitrakakis <christos.dimitrakakis@gmail.com>
2023-03-28 08:56:52 +0200

    Sarsa

---
+++ src/RL/Sarsa.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+class Sarsa:
+    def __init__(self, n_actions, n_states, discount=0.9, alpha = 0.01, epsilon=0.1, decay = 0):
+        self.n_actions = n_actions
+        self.n_states = n_states
+        self.Q = np.zeros([n_states, n_actions])
+        self.discount = discount
+        self.alpha = alpha
+        self.epsilon = epsilon
+        self.count = 0
+        self.decay = decay
+    def act(self):
+        ## by default, act greedily
+        if (np.random.uniform() < self.epsilon):
+            return np.random.randint(self.n_actions)
+        return np.argmax(self.Q[self.state, :])
+    
+    def update(self, action, reward, state):
+        ## fill in
+
+    def reset(self, state):
+        self.state = state
